b0eca4ca1a5666b23c593f2b9678097b45a77846
Cemil Buğra Uzunkaya <uzunkayacemil18@gmail.com>
2023-11-22 22:33:33 +0300

    Initial Commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
