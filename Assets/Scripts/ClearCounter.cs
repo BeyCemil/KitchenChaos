d1c2416766cf510293868773eab9552c5f25f24c
Cemil Buğra Uzunkaya <uzunkayacemil18@gmail.com>
2023-11-26 12:04:04 +0300

    Container System, Pick Up/Drop Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,51 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
 
 
 
    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-   [SerializeField] private Transform counterTopPoint;
 
-   private KitchenObject kitchenObject;
-
-   public void Interact(Player player)
+   public override void Interact(Player player)
    {
-      if (kitchenObject == null)
+      if (!HasKitchenObject())
       {
-         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+         //No Object
+         if (player.HasKitchenObject())
+         {
+            //Player Carrying
+            player.GetKitchenObject().SetKitchenObjectParent(this);
+         }
+         else
+         {
+            //Player not Carrying
+         }
       }
       else
       {
-         kitchenObject.SetKitchenObjectParent(player);
+         //Object
+         if (player.HasKitchenObject())
+         {
+            //Carrying
+         }
+         else
+         {
+            //Not Carrying
+            GetKitchenObject().SetKitchenObjectParent(player);       
+         }
       }
-   }
-
-   public Transform GetKitchenObjectFollowTransform()
-   {
-      return counterTopPoint;
-   }
-
-   public void SetKitchenObject(KitchenObject kitchenObject)
-   {
-      this.kitchenObject = kitchenObject;
-   }
-
-   public KitchenObject GetKitchenObject()
-   {
-      return kitchenObject;
-   }
-
-   public void ClearKitchenObject()
-   {
-      kitchenObject = null;
-   }
-
-   public bool HasKitchenObject()
-   {
-      return kitchenObject != null;
    }
 }
--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class SelectedCounterVisual : MonoBehaviour
 {
 
-    [SerializeField] private ClearCounter clearCounter;
-    [SerializeField] private GameObject visualGameObject;
+    [SerializeField] private BaseCounter baseCounter;
+    [SerializeField] private GameObject[] visualGameObjectArray;
     private void Start()
     {
         Player.Instance.OnSelectedCounterChanged += Instance_OnSelectedCounterChanged;
@@ -15,10 +15,11 @@ public class SelectedCounterVisual : MonoBehaviour
 
     private void Instance_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e)
     {
-        if (e.selectedCounter == clearCounter)
+        if (e.selectedCounter == baseCounter)
         {
             Show();
-        } else
+        }
+        else
         {
             Hide();
         }
@@ -26,11 +27,17 @@ public class SelectedCounterVisual : MonoBehaviour
 
     private void Show()
     {
-        visualGameObject.SetActive(true);
+        foreach (GameObject visualGameObject in visualGameObjectArray)
+        {
+            visualGameObject.SetActive(true);
+        }
     }
 
     private void Hide()
     {
-        visualGameObject.SetActive(false);
+        foreach (GameObject visualGameObject in visualGameObjectArray)
+        {
+            visualGameObject.SetActive(false);
+        }
     }
 }
