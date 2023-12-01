11cf91b77394af6406715fc05e28f1949c4e7cb4
Cemil Buğra Uzunkaya <uzunkayacemil18@gmail.com>
2023-12-01 21:38:03 +0300

    SceneManager & Scenes

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Button playButton;
+    [SerializeField] private UnityEngine.UI.Button quitButton;
+
+    private void Awake() 
+    {
+        playButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
+}
